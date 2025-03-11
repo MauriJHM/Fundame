6cc10b8fa78854d06d41912adf2158e83b6f085e
MauriJHM <mauriciohernandezmoguel@hotmail.com>
2025-03-10 19:25:04 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,3 +11,21 @@ except FileNotFoundError:
   st.error("El archivo 'SalidaFinalVentas.xlsx' no se encontró.")
 except Exception as e:
   st.error(f"Ocurrió un error al leer el archivo: {e}")
+
+# Lee el archivo Excel
+try:
+  df = pd.read_excel('SalidaFinalVentas.xlsx')
+  
+  # Verifica si la columna 'Region' existe en el DataFrame
+  if 'Region' in df.columns:
+    # Crea la gráfica de ventas por región
+    fig = px.bar(df, x='Region', y='Sales', title='Ventas por Región') # Reemplaza 'Ventas' con el nombre de tu columna de ventas
+    st.plotly_chart(fig)
+  else:
+      st.error("La columna 'Region' no se encuentra en el archivo.")
+  
+  st.dataframe(df) # Muestra el DataFrame en Streamlit
+except FileNotFoundError:
+  st.error("El archivo 'SalidaFinalVentas.xlsx' no se encontró.")
+except Exception as e:
+  st.error(f"Ocurrió un error al leer el archivo: {e}")
